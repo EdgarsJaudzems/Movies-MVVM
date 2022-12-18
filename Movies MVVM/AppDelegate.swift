c716e43d905f7961f26721bb9d3b782e0ac136c4
Edgars Jaudzems <73849363+EdgarsJaudzems@users.noreply.github.com>
2022-12-18 10:33:20 +0200

    Initial Commit

---
+++ Movies MVVM/AppDelegate.swift
@@ -0,0 +1,28 @@
+//
+//  AppDelegate.swift
+//  Movies MVVM
+//
+//  Created by edgars.jaudzems on 17/12/2022.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        
+        // Setting up new custom entry point
+        let window = UIWindow(frame: UIScreen.main.bounds)
+        let navigationController = UINavigationController(rootViewController: MainViewController())
+        window.rootViewController = navigationController
+        window.makeKeyAndVisible()
+        self.window = window
+        
+        return true
+    }
+}
+
---
+++ Movies MVVM/MainViewController/MainViewController.swift
@@ -0,0 +1,16 @@
+//
+//  MainViewController.swift
+//  Movies MVVM
+//
+//  Created by edgars.jaudzems on 18/12/2022.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
